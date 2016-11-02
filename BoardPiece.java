d3d8950bca87adeee04b9e478d7a47dc29af6274
Alex <alexchoy2345@gmail.com>
2016-11-01 20:42:04 -0700

    My Board Piece Class part 2

--- BoardPiece.java
+++ BoardPiece.java
@@ -7,26 +7,7 @@ import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-//  w  w  w  .  j a  v  a2s . c  o m
 public class BoardPiece {
-	/*
-	 * public static void main(String[] args) { Application.launch(args); }
-	 */
-	/*
-	 * @Override public void start(Stage primaryStage) {
-	 * primaryStage.setTitle(""); Group root = new Group(); Scene scene = new
-	 * Scene(root, 300, 250, Color.WHITE);
-	 * 
-	 * 
-	 * Rectangle r = new Rectangle(); r.setWidth(100); r.setHeight(100);
-	 * r.setFill(Color.BLUE);
-	 * 
-	 * Ellipse e = new Ellipse(); e.setCenterX(50); e.setCenterY(50);
-	 * e.setRadiusX(40); e.setRadiusY(40); e.setFill(Color.WHITE);
-	 * 
-	 * root.getChildren().add(r); root.getChildren().add(e);
-	 * primaryStage.setScene(scene); primaryStage.show(); }
-	 */
 
 	int row;
 	int column;
